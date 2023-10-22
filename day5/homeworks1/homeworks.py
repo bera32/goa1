fbc6200144cc5ec79016035574225869f1e194d8
bera32 <139687400+bera32@users.noreply.github.com>
2023-10-22 10:17:11 +0200

    Add files via upload

---
+++ day5/homeworks1/homeworks.py
@@ -0,0 +1,23 @@
+# name = input ("enter your name: ")
+# age = int(input("enter your age: "))
+# year = int(input("enter year: "))
+
+# for i in range(10):
+#     print(name + " შენ იქნები " + str(age + i) + " წლის " + str(year + i))
+
+# name = input("enter your name: ")
+# age = int(input ("enter your age: "))
+# year = int(input("enter year: "))
+
+# for i in range (5):
+#     print(name + " შენ იქნები " + str( age + i) +  " წლის " + str(year + i))
+
+
+# name = input("enter your name: ")
+# surname = input("enter your surname: ")
+# age = int(input("enter your age: "))
+# year = int(input("enter year: "))
+# for i in range (10):
+#     print(name + surname + " შენ იქნები " + str(age + i) + " წლის " + str(year +i))
+
+
