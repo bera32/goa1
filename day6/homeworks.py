7d0bfa217938e4e16b3177ed9cea2757700b3f0f
bera32 <139687400+bera32@users.noreply.github.com>
2023-10-22 10:17:28 +0200

    Add files via upload

---
+++ day6/homeworks.py
@@ -0,0 +1,55 @@
+# მომხმარებელს შემოატანინეთ ასაკი, შემოტანილი
+# ასაკი გაამრავლეთ 7 ზე და გაყეთ  3 ზე და მიღებულ ასაკს დაუმატეთ  4 სა და ცხრის ნამრავლის ჯამი
+
+
+# i = 1
+# while i < 30:
+#     print(i,"კენტია")
+#     print(i +3,"ლუწია") 
+#     i += 2
+
+# my_age = int(input("enter your age: ")) #32
+# father_age =int(input("enter your father age: ")) # 62
+# year = int(input("enter year:")) #2023
+
+# for i in range (5):
+#     print(str(year + i) + "წელს მამაჩემი ჩემზე იქნება" + str
+#     ((father_age + i ) / (my_age + i)) + "- ჯერ დიდი")
+
+# car_price = 7000 
+# new_price =7000 * 62 / 100
+# print(new_price) # არ მუშაობს
+
+# მომხმარებელს შეეკითხეთ ხელფასი
+
+# თუ 10000ზე მეტია, დაუპრინტეთ, გოა-ში სწავლობდი?
+
+# თუ 1000----10000 -ია , დაუპრინტეთ you mid
+
+# თუ 1000-ზე დაბალია, დაუპრინტეთ, შემოსულიყავი გოაში, მატრიცელო
+
+# user_salary = int(input("enter your salary: "))
+# if user_salary > 10000:
+#     print("goa-ში სწავლობდი?")
+
+# elif user_salary >= 1000 and user_salary <= 10000:
+#     print("you mid")
+
+# else:
+#     print("შემოსულიყავი goa- ში, მატრიცელო")
+
+# worker_salary = int(input("enter your salary: "))
+# if  worker_salary > 10000:
+#     print("გოაშ ი სწავლობდ ი?")
+# elif worker_salary > 1000 and worker_salary < 10000:
+#     print("you mid")
+# else:
+#     print("შემოსულიყავი გოაში მატრიცელო")
+
+#შექმენით სია სადაც გადასცემთ რაზმის ლიდერების სახელსა და
+#გვარს ელემენტებად და გამოიტანთ თქვენი რაზმის ლიდერის სახელს
+
+# შექმენით თქვენი რაზმის წევრებისაგან სია
+#დაპრინტეთ შუა 2 წევრი
+my_str = "nika 11 keshelava"
+print(int (my_str[5]) + int(my_str[6] + "4"))
