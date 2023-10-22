72128ce3178c21033634c599382709c4ea56d830
bera32 <139687400+bera32@users.noreply.github.com>
2023-10-22 10:16:53 +0200

    Add files via upload

---
+++ day4/classworks/classworks1.py
@@ -0,0 +1,29 @@
+#my_age = 32
+
+# mom_age +=5
+# ded_age +=5
+# my_age += 5
+# print (mom_age, ded_age, my_age)
+
+
+# my_age = 32
+# my_friend_age =32
+# my_son_age =3
+
+# my_age += 5
+# my_friend_age += 5
+# my_son_age += 5
+
+# print(my_age,my_friend_age,my_son_age)
+
+age = 5
+his_age = 8
+her_age = 10
+
+age += 3
+his_age += 3
+her_age += 3
+
+print(age,his_age,her_age)
+
+
